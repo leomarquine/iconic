cba0aa59182636d69b3c78fe9776ef3f9a6496c6
Leonardo Marquine <leonardomarquine@gmail.com>
2017-05-28 21:24:06 -0300

    Add Icon class

---
+++ src/Icon.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Marquine\Iconic;
+
+class Icon
+{
+    /**
+     * Current icon.
+     *
+     * @var string
+     */
+    protected $icon;
+
+    /**
+     * Icons repository.
+     *
+     * @var \Marquine\Iconic\Repository
+     */
+    protected $repository;
+
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config;
+
+    /**
+     * Create a new Icon instance.
+     *
+     * @param  \Marquine\Iconic\Repository  $repository
+     * @param  array  $config
+     * @return void
+     */
+    public function __construct(Repository $repository, $config)
+    {
+        $this->repository = $repository;
+        $this->config = $config;
+    }
+
+    /**
+     * Set the svg for the icon.
+     *
+     * @param  string  $name
+     * @return $this
+     */
+    public function make($name)
+    {
+        $this->icon = $this->repository->get(
+            $name, $this->config['path']
+        );
+
+        return $this;
+    }
+
+    /**
+     * Get the svg string.
+     *
+     * @return string
+     */
+    public function render()
+    {
+        return $this->icon;
+    }
+
+    /**
+     * Get the svg string.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->render();
+    }
+
+    /**
+     * Set the icon height.
+     *
+     * @param  mixed  $height
+     * @return $this
+     */
+    public function height($height)
+    {
+        $this->icon = preg_replace(
+            '/(?<=\sheight=")(.*?)(?=")/', $height, $this->icon
+        );
+
+        return $this;
+    }
+
+    /**
+     * Set the icon width.
+     *
+     * @param  mixed  $width
+     * @return $this
+     */
+    public function width($width)
+    {
+        $this->icon = preg_replace(
+            '/(?<=\swidth=")(.*?)(?=")/', $width, $this->icon
+        );
+
+        return $this;
+    }
+
+    /**
+     * Set the icon height and width.
+     *
+     * @param  mixed  $size
+     * @return $this
+     */
+    public function size($size)
+    {
+        return $this->height($size)->width($size);
+    }
+
+    /**
+     * Set the icon color.
+     *
+     * @param  string  $color
+     * @return $this
+     */
+    public function color($color)
+    {
+        $this->icon = preg_replace(
+            '/(?<=\sfill=")(.*?)(?=")/', $color, $this->icon
+        );
+
+        return $this;
+    }
+}
---
+++ tests/IconTest.php
@@ -0,0 +1,70 @@
+<?php
+
+use Marquine\Iconic\Icon;
+use PHPUnit\Framework\TestCase;
+
+class IconTest extends TestCase
+{
+    protected $icon;
+
+    protected $svg = '<svg fill="#000000" height="24" width="24"></svg>';
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $repository = Mockery::mock('Marquine\Iconic\Repository');
+        $repository->shouldReceive('get')->with('icon', '/path/to/icons')->once()->andReturn($this->svg);
+
+        $config = [
+            'path' => '/path/to/icons',
+        ];
+
+        $icon = new Icon($repository, $config);
+
+        $this->icon = $icon->make('icon');
+    }
+
+    /** @test */
+    function it_renders_the_svg()
+    {
+        $this->assertEquals($this->svg, $this->icon->render());
+    }
+
+    /** @test */
+    function it_casts_the_icon_instance_to_string()
+    {
+        $this->assertEquals($this->svg, $this->icon);
+    }
+
+    /** @test */
+    function it_changes_the_height_of_the_svg()
+    {
+        $svg = '<svg fill="#000000" height="16" width="24"></svg>';
+
+        $this->assertEquals($svg, $this->icon->height(16));
+    }
+
+    /** @test */
+    function it_changes_the_width_of_the_svg()
+    {
+        $svg = '<svg fill="#000000" height="24" width="16"></svg>';
+
+        $this->assertEquals($svg, $this->icon->width(16));
+    }
+
+    /** @test */
+    function it_changes_the_height_and_width_of_the_svg()
+    {
+        $svg = '<svg fill="#000000" height="16" width="16"></svg>';
+
+        $this->assertEquals($svg, $this->icon->size(16));
+    }
+
+    /** @test */
+    function it_changes_the_color_of_the_svg()
+    {
+        $svg = '<svg fill="#616e61" height="24" width="24"></svg>';
+
+        $this->assertEquals($svg, $this->icon->color('#616e61'));
+    }
