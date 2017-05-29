5dbc537dc709640b5baed355f72267a2aeab9a47
Leonardo Marquine <leonardomarquine@gmail.com>
2017-05-29 13:58:19 -0300

    Use virtual filesystem in the tests

--- tests/RepositoryTest.php
+++ tests/RepositoryTest.php
@@ -2,6 +2,7 @@
 
 use Marquine\Iconic\Repository;
 use PHPUnit\Framework\TestCase;
+use VirtualFileSystem\FileSystem;
 
 class RepositoryTest extends TestCase
 {
@@ -12,7 +13,17 @@ class RepositoryTest extends TestCase
 
         $svg  = '<svg fill="#000000" height="24" width="24"></svg>';
 
-        $this->assertEquals($svg, $repository->get('icon', __DIR__));
-        $this->assertEquals($svg, $repository->get('icon', ''));
+        $filesystem = new FileSystem();
+        mkdir($filesystem->path('icons'));
+
+        file_put_contents($filesystem->path('icons/icon.svg'), $svg);
+        $this->assertEquals(
+            $svg, $repository->get('icon', $filesystem->path('icons'))
+        );
+
+        unlink($filesystem->path('icons/icon.svg'));
+        $this->assertEquals(
+            $svg, $repository->get('icon', $filesystem->path('icons'))
+        );
     }
 }
